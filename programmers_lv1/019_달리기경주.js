99c803127e2ac6746e98cb0732b3ea3870f1aee2
JoWonYeong <wswy17@gmail.com>
2023-04-15 22:03:29 +0900

    2023-04-15 : 런타임에러 해결

--- programmers_lv1/019_달리기경주.js
+++ programmers_lv1/019_달리기경주.js
@@ -1,16 +1,31 @@
 function solution(players, callings) {
   var answer = [];
 
+  let map = new Map();
+
+  players.forEach((a, i) => {
+    map.set(a, i);
+  });
+
   callings.forEach((a, i) => {
-    let index = players.indexOf(a);
+    let index = map.get(a);
     let temp = players[index - 1];
     players[index - 1] = a;
     players[index] = temp;
+    map.set(a, index - 1);
+    map.set(temp, index);
   });
 
-  console.log(players);
+  for (let [key, value] of map.entries()) {
+    answer[value] = key;
+  }
 
   return answer;
 }
 
-solution(['mumu', 'soe', 'poe', 'kai', 'mine'], ['kai', 'kai', 'mine', 'mine']);
+console.log(
+  solution(
+    ['mumu', 'soe', 'poe', 'kai', 'mine'],
+    ['kai', 'kai', 'mine', 'mine']
+  )
+);
