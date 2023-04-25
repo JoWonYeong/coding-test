899c00d7fe7d7e297d49c2fb9ee24ee5ce2ce1d4
JoWonYeong <wswy17@gmail.com>
2023-04-26 00:52:05 +0900

    2023-04-25 : lv1 1문제

---
+++ programmers_lv1/030_문자열_다루기_기본.js
@@ -0,0 +1,19 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12918
+
+function solution(s) {
+  let answer = true;
+  if (s.length === 4 || s.length === 6) {
+    s.split('').forEach((a) => {
+      if (isNaN(a)) {
+        answer = false;
+      }
+    });
+  } else {
+    answer = false;
+  }
+  return answer;
+}
+
+console.log(solution('a234'));
+console.log(solution('2f34'));
+console.log(solution('1234'));
