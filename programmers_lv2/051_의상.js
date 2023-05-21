10eae871575e0290f484908a9a4d2385574c3b12
JoWonYeong <wswy17@gmail.com>
2023-05-22 00:04:13 +0900

    2023-05-21 : lv2 2문제

---
+++ programmers_lv2/051_의상.js
@@ -0,0 +1,28 @@
+function solution(clothes) {
+  var answer = 1;
+  let map = new Map();
+
+  clothes.forEach((a, i) => {
+    map.has(a[1])
+      ? map.set(a[1], [...map.get(a[1]), a[0]])
+      : map.set(a[1], [a[0]]);
+  });
+
+  map.forEach((a, i) => {
+    answer *= a.length + 1;
+  });
+
+  return answer - 1;
+}
+
+solution([
+  ['yellow_hat', 'headgear'],
+  ['blue_sunglasses', 'eyewear'],
+  ['green_turban', 'headgear'],
+]);
+
+// solution([
+//   ['crow_mask', 'face'],
+//   ['blue_sunglasses', 'face'],
+//   ['smoky_makeup', 'face'],
+// ]);
---
+++ programmers_lv2/051_튜플.js
@@ -0,0 +1,21 @@
+function solution(s) {
+  let arr = JSON.parse(s.replace(/{/g, '[').replace(/}/g, ']'));
+  let map = new Map();
+  let temp = [];
+
+  arr.forEach((a, i) => {
+    map.set(a.length, a);
+  });
+
+  for (let i = 1; i <= map.size; i++) {
+    temp.push(...map.get(i));
+  }
+
+  return [...new Set(temp)];
+}
+
+solution('{{2},{2,1},{2,1,3},{2,1,3,4}}');
+solution('{{1,2,3},{2,1},{1,2,4,3},{2}}');
+solution('{{20,111},{111}}');
+solution('{{123}}');
+solution('{{4,2,3},{3},{2,3,4,1},{2,3}}');
