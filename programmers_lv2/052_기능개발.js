021e7a73c3402fb602c044a080b3ae9dc59fd9ab
JoWonYeong <wswy17@gmail.com>
2023-05-22 22:34:10 +0900

    2023-05-22 : lv2 2문제

---
+++ programmers_lv2/052_기능개발.js
@@ -0,0 +1,22 @@
+function solution(progresses, speeds) {
+  var answer = [];
+  let index = 0;
+
+  while (index < progresses.length) {
+    let count = 0;
+    for (let i = 0; i < progresses.length; i++) {
+      progresses[i] += speeds[i];
+    }
+    while (progresses[index] >= 100) {
+      count++;
+      index++;
+    }
+
+    count !== 0 && answer.push(count);
+  }
+
+  return answer;
+}
+
+solution([93, 30, 55], [1, 30, 5]);
+// solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]);
---
+++ programmers_lv2/052_프로세스.js
@@ -0,0 +1,29 @@
+function solution(priorities, location) {
+  var answer = 0;
+  const 원본배열 = [...priorities];
+  let index = 0;
+
+  priorities.sort((a, b) => b - a);
+
+  while (true) {
+    if (priorities[index] === 원본배열[0]) {
+      원본배열.shift();
+      answer++;
+      if (location === 0) break;
+      index++;
+      location--;
+    } else {
+      원본배열.push(원본배열.shift());
+      if (location === 0) {
+        location = 원본배열.length - 1;
+      } else {
+        location--;
+      }
+    }
+  }
+
+  return answer;
+}
+
+// solution([2, 1, 3, 2], 2);
+solution([1, 1, 9, 1, 1, 1], 0);
