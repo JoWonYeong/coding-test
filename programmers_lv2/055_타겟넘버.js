8811cf33b77839432c2730dbad5e6591b1574fe3
JoWonYeong <wswy17@gmail.com>
2023-05-28 02:32:03 +0900

    2023-05-27 : lv2 1문제

---
+++ programmers_lv2/055_타겟넘버.js
@@ -0,0 +1,20 @@
+function solution(numbers, target) {
+  var answer = 0;
+
+  function dfs(현재인덱스, sum) {
+    // 종료조건
+    if (현재인덱스 === numbers.length) {
+      if (sum === target) answer++;
+    } else {
+      dfs(현재인덱스 + 1, sum + numbers[현재인덱스]);
+      dfs(현재인덱스 + 1, sum - numbers[현재인덱스]);
+    }
+  }
+
+  dfs(0, 0);
+
+  return answer;
+}
+
+solution([1, 1, 1, 1, 1], 3);
+solution([4, 1, 2, 1], 4);
