26540f075d74fd436ab80d92e158eee582a1627c
JoWonYeong <wswy17@gmail.com>
2023-07-03 22:00:01 +0900

    2023-07-03 : lv2 1문제

---
+++ programmers_lv2/067_뒤에있는큰수찾기.js
@@ -0,0 +1,15 @@
+function solution(numbers) {
+  const answer = new Array(numbers.length).fill(-1);
+  const stack = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    while (stack.length && numbers[stack[stack.length - 1]] < numbers[i]) {
+      answer[stack.pop()] = numbers[i];
+    }
+    stack.push(i);
+  }
+  return answer;
+}
+
+solution([2, 3, 3, 5]);
+solution([9, 1, 5, 3, 6, 2]);
