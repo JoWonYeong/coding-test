65fc90b343d7d3a1ad7a0f99a32c504d27b2c8f4
JoWonYeong <wswy17@gmail.com>
2023-07-07 22:15:24 +0900

    2023-07-07 : lv2 1문제 덜품

--- programmers_lv2/068_1차_프렌즈4블록.js
+++ programmers_lv2/068_1차_프렌즈4블록.js
@@ -1,4 +1,26 @@
 function solution(m, n, board) {
+  // m : 높이(세로), n : 폭(가로)
+  let curBoard = [...board];
+
+  for (let i = 0; i < m - 1; i++) {
+    for (let j = 0; j < n; j++) {
+      if (
+        board[i][j] === board[i][j + 1] &&
+        board[i][j] === board[i + 1][j] &&
+        board[i][j] === board[i + 1][j + 1]
+      ) {
+        console.log(curBoard[i][j]);
+        curBoard[i][j + 1] = curBoard[i + 1][j] = curBoard[i + 1][j + 1] = '';
+      }
+    }
+  }
+  console.log(board);
+
+  console.log(curBoard);
+
   var answer = 0;
   return answer;
 }
+
+// solution(6, 6, ['TTTANT', 'RRFACC', 'RRRFCC', 'TRRRAA', 'TTMMMF', 'TMMTTJ']);
+solution(4, 5, ['CCBDE', 'AAADE', 'AAABF', 'CCBBF']);
