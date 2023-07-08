fd40a12b1ea1fc43d1ced3000f9f7dd45af74f26
JoWonYeong <wswy17@gmail.com>
2023-07-08 14:50:17 +0900

    2023-07-08 : lv2 1문제

--- programmers_lv2/068_1차_프렌즈4블록.js
+++ programmers_lv2/068_1차_프렌즈4블록.js
@@ -1,26 +1,53 @@
 function solution(m, n, board) {
+  let answer = 0;
   // m : 높이(세로), n : 폭(가로)
-  let curBoard = [...board];
+  board = board.map((v) => v.split(''));
 
-  for (let i = 0; i < m - 1; i++) {
-    for (let j = 0; j < n; j++) {
-      if (
-        board[i][j] === board[i][j + 1] &&
-        board[i][j] === board[i + 1][j] &&
-        board[i][j] === board[i + 1][j + 1]
-      ) {
-        console.log(curBoard[i][j]);
-        curBoard[i][j + 1] = curBoard[i + 1][j] = curBoard[i + 1][j + 1] = '';
+  while (true) {
+    // 4칸 만들어지는 시작 인덱스 채워넣기
+    let indexArr = [];
+    let isEnd = true;
+
+    for (let i = 0; i < m - 1; i++) {
+      for (let j = 0; j < n; j++) {
+        if (
+          board[i][j] !== ' ' &&
+          board[i][j] === board[i][j + 1] &&
+          board[i][j] === board[i + 1][j] &&
+          board[i][j] === board[i + 1][j + 1]
+        ) {
+          isEnd = false;
+          indexArr.push([i, j]);
+        }
+      }
+    }
+    if (isEnd) break;
+    answer = 0;
+
+    // 4칸 인덱스 돌면서 블록 지우기
+    indexArr.forEach((a, i) => {
+      board[a[0]][a[1]] =
+        board[a[0] + 1][a[1]] =
+        board[a[0]][a[1] + 1] =
+        board[a[0] + 1][a[1] + 1] =
+          ' ';
+    });
+
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) {
+        if (board[j][i] === ' ') {
+          // 지워진 블록 갯수+1
+          answer++;
+          for (let k = j; k > 0; k--) {
+            board[k][i] = board[k - 1][i];
+            board[k - 1][i] = ' ';
+          }
+        }
       }
     }
   }
-  console.log(board);
-
-  console.log(curBoard);
-
-  var answer = 0;
   return answer;
 }
 
-// solution(6, 6, ['TTTANT', 'RRFACC', 'RRRFCC', 'TRRRAA', 'TTMMMF', 'TMMTTJ']);
 solution(4, 5, ['CCBDE', 'AAADE', 'AAABF', 'CCBBF']);
+solution(6, 6, ['TTTANT', 'RRFACC', 'RRRFCC', 'TRRRAA', 'TTMMMF', 'TMMTTJ']);
